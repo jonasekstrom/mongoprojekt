00e044b565c964efe3a6e396105da76ff3942792
thatzita <robinudehn@gmail.com>
2018-10-04 11:18:08 +0200

    express

--- server.js
+++ server.js
@@ -2,11 +2,35 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+
 app.get("/", (req,res) => {
     console.log("requsted URL: " + req.url);
-    res.send("thatzita")
+    res.send("Default");
+});
+
+app.post("/login", (req,res) => {
+    console.log("requsted URL: " + req.url);
+    // REQ = username, email, pw
+    res.send("Login");
+});
+
+app.post("/create", (req,res) => {
+    console.log("requsted URL: " + req.url);
+    // REQ = username, email, pw
+    res.send("Post user to DB");
+});
+
+app.get("/playlist", (req,res) => {
+    console.log("requsted URL: " + req.url);
+    res.send("fetch all the music");
+});
+
+app.get("/search", (req,res) => {
+    console.log("requsted URL: " + req.url);
+    // REQ - genres, username, playlist name
+    res.send("Search");
 });
 
 app.listen(port, () => {
-    console.log("Listening on port 5000");
+    console.log("Listening on port " + port );
 });
